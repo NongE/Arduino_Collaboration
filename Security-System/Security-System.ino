eb0ca9d0b12e5315a9826a89507bc4844b0e3562
NongE <hsb9504@gmail.com>
2019-11-05 15:53:38 +0900

    calArea - 현재 비밀번호와 입력된 번호의 넓이를 구하고 비교하여 결과를 반환하도록 변경 - 기능 변경에 따라 passwd클래스에서 불필요 변수 및 함수 삭제 calDiffPoint - 현재 비밀번호와 입력된 번호의 좌표관의 관계를 구하고 비교하여 결과를 반환하도록 변경 - 기능 변경에 따라 passwd클래스에서 불필요 변수 및 함수 삭제 checkAll - calArea, calDiffPoint 기능 변경으로 인해 해당 함수 삭제 printPoint - 좌표 출력을 위한 함수 추가

--- Security-System/Security-System.ino
+++ Security-System/Security-System.ino
@@ -1,4 +1,3 @@
-
 /*************return 값을 2개 반환하기 위해 구조체 생성 ************/
 struct values {
 
@@ -33,51 +32,66 @@ class pointXY {
 /*************비밀번호 클래스 ************/
 class passwd {
 
-  private:
-    float area; // 세점으로하는 삼각형의 넓이
-    pointXY diffPoint[2]; // 한 점을 기준으로 나머지 점의 차를 저장
 
   public:
     pointXY passwdXY[3]; // 비밀번호 좌표를 저장하는 변수
-    
-    void setArea(float area) {
-      this->area = area;
-
-    }
-    void setDiffPoint(pointXY diffPoint[]) {
-      this->diffPoint[0] = diffPoint[0];
-      this->diffPoint[1] = diffPoint[1];
-    }
-
-    float getArea() {
-      return area;
-    }
-
-    pointXY* getDiffPoint() {
-      return diffPoint;
-    }
-
 
 
 };
 
 /*************좌표 간의 넓이 계산 ************/
-float calArea(pointXY ptXY[]) {
+float calArea(pointXY ptXY[], pointXY ptXY2[]) {
   // (ptXY[0].x, ptXY[0].y)  (ptXY[1].x,ptXY[1].y)   (ptXY[2].x, ptXY[2].y)
   //     a         b             c         d              e         f
 
   float area = 0.5 * ((ptXY[0].getX() * ptXY[1].getY() + ptXY[1].getX() * ptXY[2].getY() + ptXY[2].getX() * ptXY[0].getY())
                       - (ptXY[1].getX() * ptXY[0].getY() + ptXY[2].getX() * ptXY[1].getY() + ptXY[0].getX() * ptXY[2].getY()));
-  return area;
+  float area2 = 0.5 * ((ptXY2[0].getX() * ptXY2[1].getY() + ptXY2[1].getX() * ptXY2[2].getY() + ptXY2[2].getX() * ptXY2[0].getY())
+                       - (ptXY2[1].getX() * ptXY2[0].getY() + ptXY2[2].getX() * ptXY2[1].getY() + ptXY2[0].getX() * ptXY2[2].getY()));
+
+  if (area == area2)
+  {
+    Serial.println("1차 검사 통과\n");
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+
 }
 
 /*************좌표 간의 차이 계산 ************/
-pointXY* calDiffPoint(pointXY ptXY[]) {
+pointXY* calDiffPoint(pointXY ptXY[], pointXY ptXY2[]) {
 
   pointXY diffPoint[2];
-  diffPoint[0].setXY((ptXY[0].getX() - (ptXY[0].getX())), (ptXY[0].getY() - (ptXY[0].getY())));
-  diffPoint[1].setXY((ptXY[0].getX() - (ptXY[1].getX())), (ptXY[0].getY() - (ptXY[1].getY())));
-  return diffPoint;
+  pointXY diffPoint2[2];
+
+  diffPoint[0].setXY((ptXY[0].getX() - (ptXY[1].getX())), (ptXY[0].getY() - (ptXY[1].getY())));
+  diffPoint[1].setXY((ptXY[0].getX() - (ptXY[2].getX())), (ptXY[0].getY() - (ptXY[2].getY())));
+
+  Serial.println("입력된 비밀번호의 차 입니다.");
+  printPoint(diffPoint);
+
+  diffPoint2[0].setXY((ptXY2[0].getX() - (ptXY2[1].getX())), (ptXY2[0].getY() - (ptXY2[1].getY())));
+  diffPoint2[1].setXY((ptXY2[0].getX() - (ptXY2[2].getX())), (ptXY2[0].getY() - (ptXY2[2].getY())));
+
+
+  Serial.println("비밀번호의 차 입니다.");
+  printPoint(diffPoint2);
+
+
+  if (diffPoint[0].getX() == diffPoint2[0].getX() &&
+      diffPoint[0].getY() == diffPoint2[0].getY() &&
+      diffPoint[1].getX() == diffPoint2[1].getX() &&
+      diffPoint[1].getY() == diffPoint2[1].getY())
+  {
+
+    Serial.println("2차 검사 통과\n");
+    return 1;
+  }
+  else return 0;
+
 
 }
 
@@ -95,53 +109,16 @@ int checkPrePoint(pointXY ptXY, pointXY preptXY) {
     return 0;
 }
 
-/*************비밀번호 동일여부 확인 ************/
-int checkAll(pointXY ptXY[], passwd pw) {
 
+void printPoint(pointXY ptXY[]) {
 
-  /*************넓이 비교 ************/
-
-  int index = 0;
-  pointXY tmp[2]; // 기준점으로 부터 차이를 저장하는 임시 변수
-  pointXY tmppass[2]; // 기준점으로 부터 차이를 저장하는 임시 변수
-  /*if ((calArea(ptXY) == pw.getArea())||(calArea(ptXY) == 4*pw.getArea()))*/
-  if (calArea(ptXY) == pw.getArea()) {
-    index++;
-    printf("1차 검사 통과\n");
-  } 
-
-  /*************차이 비교 ************/
-
-  tmp[0].setXY(calDiffPoint(ptXY)[0].getX(), calDiffPoint(ptXY)[0].getY());
-  tmp[1].setXY(calDiffPoint(ptXY)[1].getX(), calDiffPoint(ptXY)[1].getY());
-
-  tmppass[0].setXY(pw.getDiffPoint()[0].getX(), pw.getDiffPoint()[0].getY());
-  tmppass[1].setXY(pw.getDiffPoint()[1].getX(), pw.getDiffPoint()[1].getY());
-
-
-/* if (tmp[0].getX() == tmppass[0].getX() &&
-      tmp[0].getY() == tmppass[0].getY() &&
-     tmp[1].getX() == tmppass[1].getX() &&
-      tmp[1].getY() == tmppass[1].getY() ||
-      tmp[0].getX() == 2*tmppass[0].getX() &&
-       tmp[0].getY() == 2*tmppass[0].getY() &&
-      tmp[1].getX() == 2*tmppass[1].getX() &&
-       tmp[1].getY() == 2*tmppass[1].getY()) */
-
-  if (tmp[0].getX() == tmppass[0].getX() &&
-      tmp[0].getY() == tmppass[0].getY() &&
-      tmp[1].getX() == tmppass[1].getX() &&
-      tmp[1].getY() == tmppass[1].getY())
-  {
-    index++;
-    printf("2차 검사 통과\n");
+  for (int i = 0; i < 2; i++) {
+    Serial.print(ptXY[i].getX());
+    Serial.print(" ");
+    Serial.println(ptXY[i].getY());
   }
 
-  return index;
-
 }
-
-
 /*************전역변수 선언 ************/
 passwd pw;
 pointXY ptXY[3];
@@ -159,12 +136,10 @@ int fsrSensor8 = A8; // 9번 압력센서 A8에 연결
 
 void setup() {
   /*************초기 암호 설정(2,4,5) ************/
-  //pw.setArea(0.5);
-  pw.passwdXY[0].setXY(1, 0);
-  pw.passwdXY[1].setXY(0, -1);
+  Serial.begin(9600);
+  pw.passwdXY[0].setXY(0, 0);
+  pw.passwdXY[1].setXY(0, 1);
   pw.passwdXY[2].setXY(1, 1);
-  pw.setArea(calArea(pw.passwdXY));
-  pw.setDiffPoint(pw.passwdXY);
 
 }
 
@@ -189,21 +164,28 @@ void loop() {
 
   int fsr5 = analogRead(fsrSensor5); // A5로 부터 아날로그 신호를 읽고 이를 fsr5에 저장
   int transFsr5 = map(fsr5, 0, 1024, 0, 255); // maapping 실시
+  /*
+    int fsr6 = analogRead(fsrSensor6); // A6로 부터 아날로그 신호를 읽고 이를 fsr6에 저장
+    int transFsr6 = map(fsr6, 0, 1024, 0, 255); // maapping 실시
 
-  int fsr6 = analogRead(fsrSensor6); // A6로 부터 아날로그 신호를 읽고 이를 fsr6에 저장
-  int transFsr6 = map(fsr6, 0, 1024, 0, 255); // maapping 실시
+    int fsr7 = analogRead(fsrSensor7); // A7로 부터 아날로그 신호를 읽고 이를 fsr7에 저장
+    int transFsr7 = map(fsr7, 0, 1024, 0, 255); // maapping 실시
 
-  int fsr7 = analogRead(fsrSensor7); // A7로 부터 아날로그 신호를 읽고 이를 fsr7에 저장
-  int transFsr7 = map(fsr7, 0, 1024, 0, 255); // maapping 실시
-
-  int fsr8 = analogRead(fsrSensor8); // A8로 부터 아날로그 신호를 읽고 이를 fsr8에 저장
-  int transFsr8 = map(fsr8, 0, 1024, 0, 255); // maapping 실시
+    int fsr8 = analogRead(fsrSensor8); // A8로 부터 아날로그 신호를 읽고 이를 fsr8에 저장
+    int transFsr8 = map(fsr8, 0, 1024, 0, 255); // maapping 실시
+  */
 
   /************* 사용자가 3개의 패턴을 입력할때까지 압력센서로 입력을 받음 ************/
+
   if (pwIndex < 3)
   {
+
     if (transFsr0 > 6)
     {
+      Serial.print("현재 ");
+      Serial.print(pwIndex);
+      Serial.println("번째 암호가 입력되었습니다.");
+      Serial.println("0번");
       ptXY[pwIndex].setXY(0, 0);
       if (pwIndex != 0)
       {
@@ -213,6 +195,10 @@ void loop() {
     }
     if (transFsr1 > 6)
     {
+      Serial.print("현재 ");
+      Serial.print(pwIndex);
+      Serial.println("번째 암호가 입력되었습니다.");
+      Serial.println("1번");
       ptXY[pwIndex].setXY(1, 0);
       if (pwIndex != 0)
       {
@@ -222,6 +208,10 @@ void loop() {
     }
     if (transFsr2 > 6)
     {
+      Serial.print("현재 ");
+      Serial.print(pwIndex);
+      Serial.println("번째 암호가 입력되었습니다.");
+      Serial.println("2번");
       ptXY[pwIndex].setXY(2, 0);
       if (pwIndex != 0)
       {
@@ -231,6 +221,10 @@ void loop() {
     }
     if (transFsr3 > 6)
     {
+      Serial.print("현재 ");
+      Serial.print(pwIndex);
+      Serial.println("번째 암호가 입력되었습니다.");
+      Serial.println("3번");
       ptXY[pwIndex].setXY(0, 1);
       if (pwIndex != 0)
       {
@@ -240,6 +234,10 @@ void loop() {
     }
     if (transFsr4 > 6)
     {
+      Serial.print("현재 ");
+      Serial.print(pwIndex);
+      Serial.println("번째 암호가 입력되었습니다.");
+      Serial.println("4번");
       ptXY[pwIndex].setXY(1, 1);
       if (pwIndex != 0)
       {
@@ -249,6 +247,10 @@ void loop() {
     }
     if (transFsr5 > 6)
     {
+      Serial.print("현재 ");
+      Serial.print(pwIndex);
+      Serial.println("번째 암호가 입력되었습니다.");
+      Serial.println("5번");
       ptXY[pwIndex].setXY(2, 1);
       if (pwIndex != 0)
       {
@@ -256,43 +258,57 @@ void loop() {
       }
       pwIndex ++;
     }
-    if (transFsr6 > 6)
-    {
+    /*
+      if (transFsr6 > 6)
+      {
       ptXY[pwIndex].setXY(0, 2);
       if (pwIndex != 0)
       {
-        pwIndex += checkPrePoint(ptXY[pwIndex], ptXY[pwIndex - 1]);
+       pwIndex += checkPrePoint(ptXY[pwIndex], ptXY[pwIndex - 1]);
       }
       pwIndex ++;
-    }
-    if (transFsr7 > 6)
-    {
+      }
+      if (transFsr7 > 6)
+      {
       ptXY[pwIndex].setXY(1, 2);
       if (pwIndex != 0)
       {
-        pwIndex += checkPrePoint(ptXY[pwIndex], ptXY[pwIndex - 1]);
+       pwIndex += checkPrePoint(ptXY[pwIndex], ptXY[pwIndex - 1]);
       }
       pwIndex ++;
-    }
-    if (transFsr8 > 6)
-    {
+      }
+      if (transFsr8 > 6)
+      {
       ptXY[pwIndex].setXY(2, 2);
       if (pwIndex != 0)
       {
-        pwIndex += checkPrePoint(ptXY[pwIndex], ptXY[pwIndex - 1]);
+       pwIndex += checkPrePoint(ptXY[pwIndex], ptXY[pwIndex - 1]);
       }
       pwIndex ++;
-    }
+      }
+    */
   }
 
-  else if (pwIndex == 2)
+  else if (pwIndex == 3)
   {
-    if (checkAll(ptXY, pw) == 2)
+    Serial.println("입력된 비밀번호 입니다.");
+    printPoint(ptXY);
+
+
+    Serial.println("현재 비밀번호 입니다.");
+    printPoint(pw.passwdXY);
+
+    if (calArea(ptXY, pw.passwdXY) == 1 && calDiffPoint(ptXY, pw.passwdXY) == 1)
     {
-      printf("암호 일치\n");
+
+      Serial.println("암호 일치\n");
+      pwIndex = 0;
+      delay(1000);
     }
     else {
-      printf("암호 불일치\n");
+      Serial.println("암호 불일치\n");
+      pwIndex = 0;
+      delay(1000);
     }
   }
 
